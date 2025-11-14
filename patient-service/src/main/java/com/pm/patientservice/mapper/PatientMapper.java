5b02108fdd892a34cd8e088abdc3e34776fd7a34
Ryan Strickland <rjstrick@ncsu.edu>
2025-11-14 00:52:03 -0500

    Add service for getting list of patients

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,17 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDto;
+import com.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+
+    public static PatientResponseDto toDto(Patient patient) {
+        PatientResponseDto patientResponseDto = new PatientResponseDto();
+        patientResponseDto.setId(patient.getId().toString());
+        patientResponseDto.setName(patient.getName());
+        patientResponseDto.setAddress(patient.getAddress());
+        patientResponseDto.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientResponseDto;
+    }
+
+}
---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,26 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDto;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDto> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDto).toList();
+    }
+
+}
